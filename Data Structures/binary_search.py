5091eb35a60a50c7d14dcb7132d0467e44502f9c
adamn11 <adamnguyen73@gmail.com>
2017-07-20 09:57:16 -0500

    Fixed mixtake in algorithm and converted to python 3
    Fixed a mistake in binary_search that caused the program to output "None". Converted app to be compatible with python 3

--- Data Structures/binary_search.py
+++ Data Structures/binary_search.py
@@ -2,24 +2,25 @@ def binary_search(array, target):
     min = 0
     max = len(array)
 
-    while max > min:
-        mid = min + (max - min) / 2
+    while max >= min:
+        #mid = min + (max - min) // 2
+        mid = (max + min) // 2
 
         if array[mid] == target:
             return mid
         elif array[mid] > target:
             max = mid - 1
         elif array[mid] < target:
-            min = target + 1
+            min = mid + 1
 
 
 def main():
     # TODO write case where target is not found
     array = [0, 14, 22, 54, 87]
-    target = 2
+    target = 87
     key = binary_search(array, target)
 
-    print("Target %d is located at Index %d" % (target, key))
+    print("Target %s is located at Index %s" % (target, key))
 
 
 if __name__ == '__main__':
