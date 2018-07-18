e212d6a5eca9ceefafe3618198c3a181859f61ef
adamn11 <adamnguyen73@gmail.com>
2018-07-18 10:15:45 -0500

    Added mergesort algorithm

---
+++ Data Structures/mergesort.py
@@ -0,0 +1,39 @@
+def mergeSort(array):
+    if len(array) > 1:
+        mid = len(array) // 2
+        left = array[:mid]
+        right = array[mid:]
+
+        mergeSort(left)
+        mergeSort(right)
+
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                array[k] = left[i]
+                i += 1
+            else:
+                array[k] = right[j]
+                j += 1
+            k += 1
+        
+        while i < len(left):
+            array[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            array[k] = right[j]
+            j += 1
+            k += 1
+    return array
+
+def main():
+    unsorted = [56,24,78,13,27,98,10,41]
+    print(mergeSort(unsorted))
+    
+if __name__ == "__main__":
+    main()
