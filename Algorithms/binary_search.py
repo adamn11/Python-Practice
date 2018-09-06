3d411935f00deb60fddc5bf08e2a58d968e91663
adamn11 <adamnguyen73@gmail.com>
2018-09-06 15:23:36 -0500

    Changed all instances of variable name array to alist

--- Algorithms/binary_search.py
+++ Algorithms/binary_search.py
@@ -1,23 +1,23 @@
-def binary_search(array, target):
+def binary_search(alist, target):
     min = 0
-    max = len(array)
+    max = len(alist)
 
     while max >= min:
         #mid = min + (max - min) // 2
         mid = (max + min) // 2
 
-        if array[mid] == target:
+        if alist[mid] == target:
             return mid
-        elif array[mid] > target:
+        elif alist[mid] > target:
             max = mid - 1
-        elif array[mid] < target:
+        elif alist[mid] < target:
             min = mid + 1
 
 def main():
     # TODO write case where target is not found
-    array = [0, 14, 22, 54, 87]
+    alist = [0, 14, 22, 54, 87]
     target = 87
-    key = binary_search(array, target)
+    key = binary_search(alist, target)
 
     print("Target %s is located at Index %s" % (target, key))
 
