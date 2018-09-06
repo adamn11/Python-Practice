de8f9d54d67496979fd341aa6c4329f1f13e302b
adamn11 <adamnguyen73@gmail.com>
2018-09-06 15:34:22 -0500

    Added graph.py

---
+++ Data Structures/graph.py
@@ -0,0 +1,20 @@
+''' 
+
+Graph:
+
+Time Complexity:
+Space Complexity:
+
+Implementation:
+ DFS with adjacency list (recursive)
+ DFS with adjacency list (recursive)
+ DFS with adjacency list (iterative with stack)
+ DFS with adjacency matrix (recursive)
+ DFS with adjacency matrix (iterative with stack)
+ BFS with adjacency list
+ BFS with adjacency matrix
+ single-source shortest path (Dijkstra)
+ minimum spanning tree
+ DFS-based algorithms (see Aduni videos above):
+
+ '''
