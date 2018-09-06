24384783ad629fc0ef2e927101bc7784f5a0cf71
adamn11 <adamnguyen73@gmail.com>
2018-09-06 15:34:41 -0500

    Added heap.py

--- Data Structures/heap.py
+++ Data Structures/heap.py
@@ -16,11 +16,15 @@ Time Complexity: O(n log n)
 Space Complexity: O(1)
 
 Implementation:
-insert (O(1))
-remove (O(n log n))
-heapify_up
-heapify_down
-get_max
-get_size
+ insert
+ sift_up - needed for insert
+ get_max - returns the max item, without removing it
+ get_size() - return number of elements stored
+ is_empty() - returns true if heap contains no elements
+ extract_max - returns the max item, removing it
+ sift_down - needed for extract_max
+ remove(i) - removes item at index x
+ heapify - create a heap from an array of elements, needed for heap_sort
+ heap_sort() - take an unsorted array and turn it into a sorted array in-place using a max heap
 
 '''
