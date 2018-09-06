d3b2d47991b9582e996fa46fa4e80602cc81f5b5
adamn11 <adamnguyen73@gmail.com>
2018-09-06 09:59:52 -0500

    Added description to bubble sort

--- Algorithms/bubble_sort.py
+++ Algorithms/bubble_sort.py
@@ -1,3 +1,19 @@
+'''
+
+Bubble (Sinking) Sort: A simple sorting algorithm that repeatedly steps 
+    through the list to be sorted, compares each pair of adjacent items 
+    and swaps them if they are in the wrong order.
+
+Time Complexity (Best): O(n)
+Time Complexity (Average and Worst): O(n^2) 
+Space Complexity: O(1)
+
+Implementation:
+Bubble Sort
+
+'''
+
+
 def bubblesort(arr):
     sorted = False
 
