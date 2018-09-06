d527b26c5247abe82837f30aca9bed06ba55b7d1
adamn11 <adamnguyen73@gmail.com>
2018-09-06 09:52:34 -0500

    Added description to merge sort

--- Algorithms/merge_sort.py
+++ Algorithms/merge_sort.py
@@ -1,43 +1,57 @@
+'''
+
+Merge Sort: Sort an array, divide it into two halves, sort the two halves (recursively), 
+    and then merge the results.
+
+Time Complexity:  O(n log n)
+Space Complexity: O(n)
+
+Implementation:
+Top-Down
+Bottom-Up
+
+'''
+
 # Top-down Implementation
-def mergeSort(array):
-    if len(array) > 1:
-        mid = len(array) // 2
-        left = array[:mid]
-        right = array[mid:]
+def merge_sort_top_down(alist):
+    if len(alist) > 1:
+        mid = len(alist) // 2
+        left = alist[:mid]
+        right = alist[mid:]
 
-        mergeSort(left)
-        mergeSort(right)
+        merge_sort_top_down(left)
+        merge_sort_top_down(right)
 
-        i = 0 # Index for left array
-        j = 0 # Index for right array
-        k = 0 # Index for original array
+        i = 0 # Index for left alist
+        j = 0 # Index for right alist
+        k = 0 # Index for original alist
 
-        # Comparing left and right array and inserting the smallest one into original array
+        # Comparing left and right alist and inserting the smallest one into original alist
         while i < len(left) and j < len(right):
             if left[i] < right[j]:
-                array[k] = left[i]
+                alist[k] = left[i]
                 i += 1
             else:
-                array[k] = right[j]
+                alist[k] = right[j]
                 j += 1
             k += 1
         
-        # Inserting elements from left array when elements from right array are empty
+        # Inserting elements from left alist when elements from right alist are empty
         while i < len(left):
-            array[k] = left[i]
+            alist[k] = left[i]
             i += 1
             k += 1
 
-        # Inserting elements from right array when elements from left array are empty
+        # Inserting elements from right alist when elements from left alist are empty
         while j < len(right):
-            array[k] = right[j]
+            alist[k] = right[j]
             j += 1
             k += 1
-    return array
+    return alist
 
 def main():
     unsorted = [56,24,78,13,27,98,10,41]
-    print(mergeSort(unsorted))
+    print(merge_sort_top_down(unsorted))
     
 if __name__ == "__main__":
     main()
