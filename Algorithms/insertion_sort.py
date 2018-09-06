6d0f6ee1be833e2945b8dc54fc521b2719d661fb
adamn11 <adamnguyen73@gmail.com>
2018-09-06 10:16:32 -0500

    Resolved merge conflicts with insertion sort

--- Algorithms/insertion_sort.py
+++ Algorithms/insertion_sort.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 '''
 
 Insertion Sort: An in-place comparison-based sorting algorithm. Here, a 
@@ -37,23 +36,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-=======
-def insertionSort(alist):
-    for x in range(1,len(alist)-1):
-        value = alist[x]
-        sorted_position = x - 1
 
-        while sorted_position >= 0 and alist[sorted_position] > value:
-            alist[sorted_position+1] = alist[sorted_position]
-            sorted_position -= 1
-
-        alist[sorted_position+1] = value
-    return alist
-
-def main():
-    arr = [6,1,4,2,7,8,9]
-    print(insertionSort(arr))
-
-if __name__ == '__main__':
-    main() 
->>>>>>> ebc70956cdd07d48336152a3fb3fb54a7e048efd
