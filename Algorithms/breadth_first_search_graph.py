3419ff8eb1aeeb3cf83089434f29943ad7193bb6
adamn11 <adamnguyen73@gmail.com>
2018-10-25 22:34:52 -0500

    Added comments to breadth_first_search_graph.py

--- Algorithms/breadth_first_search_graph.py
+++ Algorithms/breadth_first_search_graph.py
@@ -1,14 +1,18 @@
 def bfs(graph, start):
-    visited = []
-    queue = [start]
+    visited = []  # A list to keep track of all the nodes that have been visited
+    queue = [start]  # BFS uses a queue, with the start node already appended
 
+    # Goes through all nodes in queue before finishing
     while queue:
         node = queue.pop(0)
 
+        # If the node has not been visited...
         if node not in visited:
+            # Mark as visited...
             visited.append(node)
             neighbors = graph[node]
 
+            # Then go through all nodes connected and append to queue
             for neighbor in neighbors:
                 queue.append(neighbor)
     return visited
