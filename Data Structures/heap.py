c5ac54d62dff12611d3438d8180e499b24d311e4
adamn11 <adamnguyen73@gmail.com>
2018-12-20 10:55:35 -0600

    Deleted duplicate comment

--- Data Structures/heap.py
+++ Data Structures/heap.py
@@ -16,33 +16,6 @@ Time Complexity: O(n log n)
 Space Complexity: O(1)
 
 Implementation:
-insert (O(1))
-remove (O(n log n))
-heapify_up
-heapify_down
-get_max
-get_size
-
-'''
-
-'''
-
-Heap: A binary tree with two special properties: 
-    Shape: A heap must be a complete binary tree in that all levels of the 
-        tree must be completely filled - except the last node) 
-    Order: A heap's root node must have all its children be less than or 
-        equal to or greater than or equal to its children)
-
-    Min Heap: Every single parent node (including the root node) is less 
-        than or equal to the value of its children nodes
-
-    Max Heap: Every single parent node (including the root node) is 
-        greater than or equal to its children nodes.
-
-Time Complexity: O(n log n)
-Space Complexity: O(1)
-
-Implementation:
  insert
  sift_up - needed for insert
  get_max - returns the max item, without removing it
